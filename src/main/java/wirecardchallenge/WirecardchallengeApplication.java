0e71e0a8e4c14a0c7e997d1f6867ad98667541aa
Renan Silva <renan.tssilva@gmail.com>
2019-01-24 20:25:44 -0200

    initial commit

---
+++ src/main/java/wirecardchallenge/WirecardchallengeApplication.java
@@ -0,0 +1,14 @@
+package wirecardchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WirecardchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WirecardchallengeApplication.class, args);
+	}
+
+}
+
