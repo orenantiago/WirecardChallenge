4407defa6ba323866dfe9ff5385ac4f3f3a2be91
Renan Silva <renan.tssilva@gmail.com>
2019-01-29 17:48:37 -0200

    improve application test

--- src/test/java/wirecardchallenge/WirecardchallengeApplicationTest.groovy
+++ src/test/java/wirecardchallenge/WirecardchallengeApplicationTest.groovy
@@ -1,8 +1,18 @@
 package wirecardchallenge
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.core.env.Environment
 import spock.lang.Specification
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class WirecardchallengeApplicationTest extends Specification {
-    def "Main"() {
+
+    @Autowired
+    Environment environment
+
+    def "application should start up"() {
+        expect:
+        environment != null
     }
 }
